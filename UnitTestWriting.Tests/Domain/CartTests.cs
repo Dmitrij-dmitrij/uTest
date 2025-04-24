7fd0589054256ce76fc51becc0d5f1380c0f833a
Ротаренко Дмитрий <rotarenko_dv@vtb.ru>
2025-04-24 12:20:07 +0300

    Revert "Добавлен тест корзины"
    This reverts commit a9e76d5611079ec31624ceaf8176489dc7320c28.

--- UnitTestWriting.Tests/Domain/CartTests.cs
+++ UnitTestWriting.Tests/Domain/CartTests.cs
@@ -1,4 +1,3 @@
-using UnitTestWriting.Domain;
 using Xunit;
 
 namespace UnitTestWriting.Tests.Domain
@@ -15,37 +14,5 @@ namespace UnitTestWriting.Tests.Domain
             // Assert
             Assert.True(true);
         }
-
-        [Fact]
-        public void Cart_15Discount_OK()
-        {
-            // Arrange
-            var cart = new Cart(new User()
-            {
-                Name = "Test",
-                UpdatedAt = DateTime.Now,
-            });
-            // Act
-            cart.Discount = 15;
-
-            // Assert
-            Assert.Equal(cart.Discount,15);
-        }
-        [Fact]
-        public void Cart_NoPromo_OK()
-        {
-            // Arrange
-            var cart = new Cart(new User()
-            {
-                Name = "Test",
-                UpdatedAt = DateTime.Now                
-            });
-            // Act
-            cart.Discount = 51;
-
-            // Assert
-            Assert.Null (cart.PromoCode);
-        }
-
     }
 }
